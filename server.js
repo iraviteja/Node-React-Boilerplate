dde4e6bc24d62a7aabf47240a1c9e2ae9ad28b05
iraviteja <iraviteja77@gmail.com>
2018-12-16 12:34:58 +0530

    running server concurrently

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 const app = express();
 
 const db = require("./config/dbConnect");
 
-const port = process.env.PORT || 5000;
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Express Validator middleware
+app.use(expressValidator());
+
+// Server connection
+const port = process.env.PORT || 5000;
 app.listen(port, err => {
   if (err) throw err;
   console.log(`Server is running in port ${port}`);
