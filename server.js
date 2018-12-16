92879c4c3ba0595f4c21064cf54bb4e9a05fa9b0
iraviteja <iraviteja77@gmail.com>
2018-12-16 11:45:49 +0530

    server started

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`Server is running in port ${port}`);
+});
