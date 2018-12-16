cbced950ba2b7226977ee24ff032d5e95cb23cd4
iraviteja <iraviteja77@gmail.com>
2018-12-16 14:55:38 +0530

    node-sass integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   render() {
-    return <h1>HELLO WORLD</h1>;
+    return (
+      <div>
+        <h1>HELLO WORLD</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    );
   }
 }
 
