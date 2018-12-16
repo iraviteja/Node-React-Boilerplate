ac1e80b4f7c26b5232ce39b6df50b08407fd4d9b
iraviteja <iraviteja77@gmail.com>
2018-12-16 17:41:54 +0530

    redux implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
+import { Provider } from "react-redux";
+
+import store from "./Store";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>HELLO WORLD</h1>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1>HELLO WORLD</h1>
+        </div>
+      </Provider>
     );
   }
 }
